b1e04a6cdca2df6ff984269913b758d351a77672
Sanjit <sanjit@sanjitchakrabarti.com>
2021-03-03 11:03:46 +0530

    Added files

---
+++ wp-content/theme/homey/single-listing/title.php
@@ -0,0 +1,52 @@
+<?php
+global $post, $homey_prefix, $listing_author;
+$address = homey_get_listing_data('listing_address');
+$price_separator = homey_option('currency_separator');
+
+$is_superhost = $listing_author['is_superhost'];
+$listing_price = homey_get_price();
+$rating = homey_option('rating');
+$total_rating = get_post_meta( $post->ID, 'listing_total_rating', true );
+$min_hours = get_post_meta($post->ID, 'homey_min_book_hours', true);
+?>
+<div class="title-section">
+    <div class="block block-top-title">
+        <div class="block-body">
+            <?php get_template_part('template-parts/breadcrumb'); ?>
+            <h1 class="listing-title">
+                <?php the_title(); ?> <?php homey_listing_featured(get_the_ID()); ?>    
+                <span class="item-price" style="text-align:center; float: right; font-weight: 600;"> 
+					<?php 	
+                    $beds       = homey_get_listing_data('beds');
+					echo '&nbsp;&nbsp;<i class="fas fa-users"></i> ' .$beds. " Total Capacity"?>  
+					</span>
+            </h1>
+         
+            <?php if(!empty($address)) { ?>
+            <address><i class="fa fa-map-marker" aria-hidden="true"></i> <?php echo esc_attr($address); ?></address>
+            <?php } ?>
+           
+            <div class="host-avatar-wrap avatar">
+           
+
+                <?php if($is_superhost) { ?>
+                <span class="super-host-icon">
+                    <i class="fa fa-bookmark"></i>
+                   
+                </span>
+                <?php } ?>
+                <?php echo ''.$listing_author['photo'];?>
+
+                
+
+            </div>
+
+            <?php if($rating && ($total_rating != '' && $total_rating != 0 ) ) { ?>
+            <div class="list-inline rating hidden-xs">
+           
+                <?php echo homey_get_review_stars($total_rating, true, true); ?>
+            </div>
+            <?php } ?>
+        </div><!-- block-body -->
+    </div><!-- block -->
+</div><!-- title-section -->
